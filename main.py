873fa41eba3a4139cbeded1c971d430595d1d069
ThiagoLozano <51161587+ThiagoLozano@users.noreply.github.com>
2020-08-22 16:17:42 -0300

    Programa

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import matplotlib.pyplot as plt
 
-# Variável Linha x:
-meses = ['Loja A',
-         'Loja B',
-         'Loja C',
-         'Loja D',
-         'Loja E',
-         'Loja F']
 
-# Variável linha y:
-valores = [120000,
-           150000,
-           145500,
-           120500,
-           134400,
-           119880]
+class Grafico:
+    def __init__(self):
+        # Linha X
+        self.lojas = ['Loja A', 'Loja B', 'Loja C',
+                      'Loja D', 'Loja E', 'Loja F']
+        # Linha y
+        self.valores = [120000, 150000, 145500,
+                        120500, 134400, 119880]
 
-# Título do gráfico.
-plt.title('Faturamento total de cada loja no ano de 2019')
+    def GerarGrafico(self):
+        # Título do gráfico.
+        plt.title('Faturamento total de cada loja no ano de 2019')
 
-# Título linha x:
-plt.xlabel('Meses')
+        # Título linha x:
+        plt.xlabel('Loja')
 
-# Título Linha y:
-plt.ylabel('Faturado (R$)')
+        # Título Linha y:
+        plt.ylabel('Faturado (R$)')
 
-# Insere as variáveis em gráfico de barra.
-plt.bar(meses, valores)
+        # Insere as variáveis em gráfico de barra.
+        plt.bar(self.lojas, self.valores)
 
-# Executa o gráfico.
-plt.show()
+        # Executa o gráfico.
+        plt.show()
+
+
+user = Grafico()
+user.GerarGrafico()
